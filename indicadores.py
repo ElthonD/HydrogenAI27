6add9c47bece04b4cecfe51d0de65ae6dda4cd2e
Elthon Daniel Rivas <60632619+ElthonD@users.noreply.github.com>
2023-08-03 16:07:14 -0600

    Update indicadores.py
    Eliminado matplotlib

--- indicadores.py
+++ indicadores.py
@@ -5,7 +5,6 @@ import pandas as pd
 import numpy as np
 from random import sample
 import plotly.express as px
-import matplotlib.pyplot as plt
 from dateutil.relativedelta import *
 import seaborn as sns; sns.set_theme()
 import plotly.graph_objects as go
