452926eae35671d0d8c86029b6817f0187866e44
Elthon Daniel Rivas <60632619+ElthonD@users.noreply.github.com>
2023-10-25 08:57:28 -0600

    Update metricas.py

--- metricas.py
+++ metricas.py
@@ -2541,14 +2541,14 @@ def createPage():
 
             data2 =  df_grafico_bases(data1)
 
-            def format_int_with_commas(x):
-                """
-                Formats an integer with commas as thousand separators.
-                """
-                return f"{x:,}"
+            #def format_int_with_commas(x):
+                #"""
+                #Formats an integer with commas as thousand separators.
+                #"""
+                #return f"{x:,}"
 
-            data2['Ingresos'] = data2['Ingresos'].applymap(format_int_with_commas)
-            data2['Ingresos Per Cápita'] = data2['Ingresos Per Cápita'].applymap(format_int_with_commas)
+            #data2['Ingresos'] = data2['Ingresos'].applymap(format_int_with_commas)
+            #data2['Ingresos Per Cápita'] = data2['Ingresos Per Cápita'].applymap(format_int_with_commas)
 
             servicios_reales_inicial = data2['Servicios Reales'].values[0]
             servicios_reales_final = data2['Servicios Reales'].values[-1]
